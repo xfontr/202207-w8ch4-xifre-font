d2bd5226bf1b75c8b208f7c31526518a3ab43103
Xifre Font <xifre.font.97@gmail.com>
2022-08-25 22:35:06 +0200

    Tested the name composer function

--- src/utils/nameGenerator.ts
+++ src/utils/nameGenerator.ts
@@ -44,8 +44,9 @@ export const randomLetterGenerator = (): string =>
 export const randomNumberGenerator = (): number =>
   Math.floor(Math.random() * (range.max - range.min + 1) + range.min);
 
-const composeName = (): (string | number)[] => {
+export const composeName = (): (string | number)[] => {
   const composedName = new Array(5).fill("");
+
   const finalComposition = composedName.map((character, index) =>
     index < 2 ? randomNumberGenerator() : randomLetterGenerator()
   );

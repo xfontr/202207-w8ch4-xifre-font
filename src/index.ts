f50fdb334c81bc8891e3d27b3394d521c0b328b3
Xifre Font <xifre.font.97@gmail.com>
2022-08-25 20:15:52 +0200

    Set up dotenv and debug

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import "loadEnvironment";
+import Debug from "debug";
 import Developer from "./RobotTypes/Developer";
 import Waiter from "./RobotTypes/Waiter";
 import Cleaner from "./RobotTypes/Cleaner";
 
+const debug = Debug("robots:index");
+
 const cleaner = new Cleaner();
-
 const developer = new Developer();
-
 const waiter = new Waiter();
 
-export { cleaner, developer, waiter };
+debug(`Created the robot ${cleaner.name} as a ${cleaner.type}`);
+debug(`Created the robot ${developer.name} as a ${developer.type}`);
+debug(`Created the robot ${waiter.name} as a ${waiter.type}`);

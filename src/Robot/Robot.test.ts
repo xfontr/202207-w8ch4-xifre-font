bba894eee50036b3da32a49500f123c4cb0e4cc8
Xifre Font <xifre.font.97@gmail.com>
2022-08-25 20:09:44 +0200

    Tested the reset method

--- src/Robot/Robot.test.ts
+++ src/Robot/Robot.test.ts
@@ -5,12 +5,28 @@ describe("Given a Robot class", () => {
   describe("When instantiated with a type 'developer'", () => {
     test("Then it should create a Robot with said type and the remaining default values", () => {
       const robotType = "developer";
-      const robot = new Robot("developer");
+      const robot = new Robot(robotType);
 
       expect(robot).toHaveProperty("name");
       expect(robot.type).toBe(robotType);
       expect(robot.message).toBe(messages.developer);
       expect(robot.battery).toBe(100);
+    });
+
+    test("Then the robot should have a reset method to change its name and get its battery back to 100", () => {
+      const robotType = "developer";
+      const robot = new Robot(robotType);
+
+      const robotName = robot.name;
+      const robotInitialBattery = robot.battery;
+
+      robot.battery = 50;
+      expect(robot.battery).toBe(50);
+
+      robot.reset();
+
+      expect(robot.name).not.toBe(robotName);
+      expect(robot.battery).toBe(robotInitialBattery);
     });
   });
 });
